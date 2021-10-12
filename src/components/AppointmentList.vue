1f73f1a7bd0a6a22029d474ff5b92ba8da71877a
Nchofon <ncho4real@gmail.com>
2021-10-12 23:52:01 +0100

    using props

---
+++ src/components/AppointmentList.vue
@@ -0,0 +1,16 @@
+<templete>
+  <div class="col-12 col-md-10 col-lg-7">
+    <div class="list-group list-group-flush">
+    <div class="list-group-item d-flex align-items-start" v-for="(item, i) in appointments" v-bind:key="i" >
+      <h4>{{item.petName}}</h4>
+      <p>{{item.aptNotes}}</p>
+    </div>
+    </div>
+  </div>
+</templete>
+<script>
+  export default {
+    name: "AppointmentList",
+    props: ["appointments"]
+  };
+</script>
